536ef7e2c2d471444f07064ec845a4e78908f56d
HonokuniTomita <tomita@honokuni-tokai.com>
2024-11-12 12:00:23 +0900

    プッシュ

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,91 @@
+import styles from './page.module.css';
+import Image from 'next/image';
+
+type News = {
+	id: string;
+	title: string;
+	category: {
+		name: string;
+	};
+	publishedAt: string;
+	createdAt: string;
+};
+
+const data: {
+	contents: News[];
+} = {
+	contents: [
+		{
+			id: '1',
+			title: '渋谷にオフィスを移転しました',
+			category: {
+				name: '更新情報',
+			},
+			publishedAt: '2023/05/19',
+			createdAt: '2023/05/19',
+		},
+		{
+			id: '2',
+			title: '渋谷にオフィスを移転しました',
+			category: {
+				name: '更新情報',
+			},
+			publishedAt: '2023/05/19',
+			createdAt: '2023/05/19',
+		},
+		{
+			id: '3',
+			title: '渋谷にオフィスを移転しました',
+			category: {
+				name: '更新情報',
+			},
+			publishedAt: '2023/05/19',
+			createdAt: '2023/05/19',
+		},
+	],
+};
+
 export default function Home() {
+	const sliceData = data.contents.slice(0, 2);
 	return (
-		<div>
-			<h1 className='title'>ともきゃんのプロフィールサイト</h1>
-			<p className='description'>ともきゃんのプロフィールを紹介します。</p>
-		</div>
+		<>
+			<section className={styles.top}>
+				<div>
+					<h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+					<p className={styles.description}>
+						私たちは市場をリードしているグローバルテックカンパニーです。
+					</p>
+				</div>
+				<Image className={styles.bgimg} src='/img-mv.jpg' alt='' width={4000} height={1200} />
+			</section>
+			<section className={styles.news}>
+				<h2 className={styles.newsTitle}>News</h2>
+				<ul>
+					{sliceData.map((article) => (
+						<li key={article.id} className={styles.list}>
+							<div className={styles.link}>
+								<Image
+									className={styles.image}
+									src='/no-image.png'
+									alt='No Image'
+									width={1200}
+									height={630}
+								/>
+								<dl className={styles.content}>
+									<dt className={styles.newsItemTitle}>{article.title}</dt>
+									<dd className={styles.meta}>
+										<span className={styles.tag}>{article.category.name}</span>
+										<span className={styles.date}>
+											<Image src='/clock.svg' alt='' width={16} height={16} priority />
+											{article.publishedAt}
+										</span>
+									</dd>
+								</dl>
+							</div>
+						</li>
+					))}
+				</ul>
+			</section>
+		</>
 	);
 }
