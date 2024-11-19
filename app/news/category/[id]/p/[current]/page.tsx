06fcadf87ec1c9dfd4df109c67de23babca5f1bd
tomocan <t.tomita.info@gmail.com>
2024-11-19 20:16:41 +0900

    7章完了

--- app/news/category/[id]/p/[current]/page.tsx
+++ app/news/category/[id]/p/[current]/page.tsx
@@ -1,31 +1,43 @@
-import { getCategoryDetail, getNewsList } from '@/app/_libs/microcms';
 import { notFound } from 'next/navigation';
+import { getCategoryDetail, getNewsList } from '@/app/_libs/microcms';
 import NewsList from '@/app/_components/NewsList';
 import Pagination from '@/app/_components/Pagination';
-import Category from '@/app/_components/Category';
 import { NEWS_LIST_LIMIT } from '@/app/_constants';
 
 type Props = {
-	params: {
-		id: string;
-	};
+  params: {
+    current: string;
+    id: string;
+  };
 };
 
 export default async function Page({ params }: Props) {
-	const category = await getCategoryDetail(params.id).catch(notFound);
+  const current = parseInt(params.current as string, 10);
 
-	const { contents: news, totalCount } = await getNewsList({
-		limit: NEWS_LIST_LIMIT,
-		filters: `category[equals]${category.id}`,
-	});
+  if (Number.isNaN(current) || current < 1) {
+    notFound();
+  }
 
-	return (
-		<>
-			<p>
-				<Category category={category} /> の一覧
-			</p>
-			<NewsList news={news} />
-			<Pagination totalCount={totalCount} basePath={`/news/category/${category.id}`} />
-		</>
-	);
+  const category = await getCategoryDetail(params.id).catch(notFound);
+
+  const { contents: news, totalCount } = await getNewsList({
+    filters: `category[equals]${category.id}`,
+    limit: NEWS_LIST_LIMIT,
+    offset: NEWS_LIST_LIMIT * (current - 1),
+  });
+
+  if (news.length === 0) {
+    notFound();
+  }
+
+  return (
+    <>
+      <NewsList news={news} />
+      <Pagination
+        totalCount={totalCount}
+        current={current}
+        basePath={`/news/category/${category.id}`}
+      />
+    </>
+  );
 }
