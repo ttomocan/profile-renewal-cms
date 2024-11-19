305e372a758eb925e168d5df327f9366d8776899
HonokuniTomita <tomita@honokuni-tokai.com>
2024-11-19 10:13:05 +0900

    7-1完了

---
+++ app/news/category/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { getCategoryDetail, getNewsList } from '@/app/_libs/microcms';
+import { notFound } from 'next/navigation';
+import NewsList from '@/app/_components/NewsList';
+import Category from '@/app/_components/Category';
+
+type Props = {
+	params: {
+		id: string;
+	};
+};
+
+export default async function Page({ params }: Props) {
+	const category = await getCategoryDetail(params.id).catch(notFound);
+
+	const { contents: news } = await getNewsList({
+		filters: `category[equals]${category.id}`,
+	});
+
+	return (
+		<>
+			<p>
+				<Category category={category} /> の一覧
+			</p>
+			<NewsList news={news} />
+		</>
+	);
+}
