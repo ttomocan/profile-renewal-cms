4d2ba0684e8bf1e986c5e7fff3917557a15dbe73
HonokuniTomita <tomita@honokuni-tokai.com>
2024-11-19 11:03:57 +0900

    7-2完了

--- app/news/p/[current]/page.tsx
+++ app/news/p/[current]/page.tsx
@@ -1,31 +1,35 @@
-import { getCategoryDetail, getNewsList } from '@/app/_libs/microcms';
 import { notFound } from 'next/navigation';
+import { getNewsList } from '@/app/_libs/microcms';
 import NewsList from '@/app/_components/NewsList';
 import Pagination from '@/app/_components/Pagination';
-import Category from '@/app/_components/Category';
 import { NEWS_LIST_LIMIT } from '@/app/_constants';
 
 type Props = {
 	params: {
-		id: string;
+		current: string;
 	};
 };
 
 export default async function Page({ params }: Props) {
-	const category = await getCategoryDetail(params.id).catch(notFound);
+	const current = parseInt(params.current, 10);
+
+	if (Number.isNaN(current) || current < 1) {
+		notFound();
+	}
 
 	const { contents: news, totalCount } = await getNewsList({
 		limit: NEWS_LIST_LIMIT,
-		filters: `category[equals]${category.id}`,
+		offset: NEWS_LIST_LIMIT * (current - 1),
 	});
+
+	if (news.length === 0) {
+		notFound();
+	}
 
 	return (
 		<>
-			<p>
-				<Category category={category} /> の一覧
-			</p>
 			<NewsList news={news} />
-			<Pagination totalCount={totalCount} basePath={`/news/category/${category.id}`} />
+			<Pagination totalCount={totalCount} current={current} />
 		</>
 	);
 }
