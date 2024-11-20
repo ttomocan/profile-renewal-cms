17505f18eba31ee9c61e260df0049cc0d1a58cb7
HonokuniTomita <tomita@honokuni-tokai.com>
2024-11-20 10:45:31 +0900

    差分反映

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -4,19 +4,19 @@ import styles from './index.module.css';
 import Menu from '../Menu';
 
 export default function Header() {
-  return (
-    <header className={styles.header}>
-      <Link href="/" className={styles.logoLink}>
-        <Image
-          src="/logo.svg"
-          alt="SIMPLE"
-          className={styles.logo}
-          width={348}
-          height={133}
-          priority
-        />
-      </Link>
-      <Menu />
-    </header>
-  );
+	return (
+		<header className={styles.header}>
+			<Link href='/' className={styles.logoLink}>
+				<Image
+					src='/logo.svg'
+					alt='SIMPLE'
+					className={styles.logo}
+					width={348}
+					height={133}
+					priority
+				/>
+			</Link>
+			<Menu />
+		</header>
+	);
 }
--- app/news/p/[current]/page.tsx
+++ app/news/p/[current]/page.tsx
@@ -5,31 +5,31 @@ import Pagination from '@/app/_components/Pagination';
 import { NEWS_LIST_LIMIT } from '@/app/_constants';
 
 type Props = {
-  params: {
-    current: string;
-  };
+	params: {
+		current: string;
+	};
 };
 
 export default async function Page({ params }: Props) {
-  const current = parseInt(params.current as string, 10);
+	const current = parseInt(params.current, 10);
 
-  if (Number.isNaN(current) || current < 1) {
-    notFound();
-  }
+	if (Number.isNaN(current) || current < 1) {
+		notFound();
+	}
 
-  const { contents: news, totalCount } = await getNewsList({
-    limit: NEWS_LIST_LIMIT,
-    offset: NEWS_LIST_LIMIT * (current - 1),
-  });
+	const { contents: news, totalCount } = await getNewsList({
+		limit: NEWS_LIST_LIMIT,
+		offset: NEWS_LIST_LIMIT * (current - 1),
+	});
 
-  if (news.length === 0) {
-    notFound();
-  }
+	if (news.length === 0) {
+		notFound();
+	}
 
-  return (
-    <>
-      <NewsList news={news} />
-      <Pagination totalCount={totalCount} current={current} />
-    </>
-  );
+	return (
+		<>
+			<NewsList news={news} />
+			<Pagination totalCount={totalCount} current={current} />
+		</>
+	);
 }
