9674d64ddf17c66cfd48fa9911f31e5e083ba893
HonokuniTomita <tomita@honokuni-tokai.com>
2024-11-26 09:29:00 +0900

    1126

--- next.config.mjs
+++ next.config.mjs
@@ -12,3 +12,21 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'tomocan.site',
+          },
+        ],
+        destination: 'https://www.tomocan.site/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
