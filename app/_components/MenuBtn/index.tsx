b9d3334216b3f666d656779f58d4498817b9abed
HonokuniTomita <tomita@honokuni-tokai.com>
2024-11-26 14:03:28 +0900

    1126

--- app/_components/MenuBtn/index.tsx
+++ app/_components/MenuBtn/index.tsx
@@ -3,7 +3,7 @@
 export default function Menu() {
   return (
     <div className="l-header__menuBtn">
-      <button className="l-header__menuBtn-button">
+      <button className="l-header__menuBtn-button" aria-label="メニューを開く">
         <span className="top"></span>
         <span className="middle"></span>
         <span className="bottom"></span>
