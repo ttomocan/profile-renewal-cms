ba34d69fa03191796b47089bb5ccc65f387293a2
HonokuniTomita <tomita@honokuni-tokai.com>
2024-11-26 10:38:50 +0900

    1126

--- app/_components/Blog/index.tsx
+++ app/_components/Blog/index.tsx
@@ -1,8 +1,15 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Blog() {
   return (
     <div className="blog">
+      <div className="blog__image">
+        <picture>
+          <source srcSet="/img/common/bg_blog_sp.jpg" media="(max-width: 767px)" />
+          <Image src="/img/common/bg_blog.jpg" alt="" width={1366} height={400} priority sizes="100vw" />
+        </picture>
+      </div>
       <div className="blog__wrap l-inner">
         <h2 className="c-heading-lv2 flipDownTrigger --color-w">
           <span className="c-heading-lv2-en">Blog</span>
