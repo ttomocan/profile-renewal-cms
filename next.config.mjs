32b5afab47b1beb7a8d2d4e947670ee5571eb3a3
HonokuniTomita <tomita@honokuni-tokai.com>
2024-11-26 09:33:36 +0900

    1126

--- next.config.mjs
+++ next.config.mjs
@@ -9,22 +9,27 @@ const nextConfig = {
     ],
   },
   trailingSlash: true,
-
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'tomocan.site',
-          },
-        ],
-        destination: 'https://www.tomocan.site/:path*',
-        permanent: true,
-      },
-    ];
-  },
 };
 
-module.exports = nextConfig;
+// リダイレクト設定を個別に定義
+async function redirects() {
+  return [
+    {
+      source: '/:path*',
+      has: [
+        {
+          type: 'host',
+          value: 'tomocan.site',
+        },
+      ],
+      destination: 'https://www.tomocan.site/:path*',
+      permanent: true,
+    },
+  ];
+}
+
+// 全体設定をエクスポート
+module.exports = {
+  ...nextConfig,
+  redirects, // リダイレクト関数を追加
+};
