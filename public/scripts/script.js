4460ae724ff7f48cc7f21ac92e346c2ef0c00a26
HonokuniTomita <tomita@honokuni-tokai.com>
2024-11-26 12:16:18 +0900

    1126

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
-script.src = '/scripts/jquery-3.7.1.js';
+script.src = '/scripts/jquery-3.7.1.min.js';
 script.onload = function () {
   $('.loading').fadeOut(400);
 
