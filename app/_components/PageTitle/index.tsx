09b99b1d4538e6ce12aabbca9032edb2573789de
HonokuniTomita <tomita@honokuni-tokai.com>
2024-11-26 12:49:10 +0900

    1126

--- app/_components/PageTitle/index.tsx
+++ app/_components/PageTitle/index.tsx
@@ -10,7 +10,7 @@ export default function PageTitle({ title, sub, img }: Props) {
   return (
     <div className="pagetitle">
       <div className="pagetitle__image">
-        <Image src={img} alt="" width={1366} height={400} priority sizes="100vw" />
+        <Image src={img} alt="" width={1366} height={400} sizes="100vw" />
       </div>
       <div className="l-inner">
         <p className="pagetitle__en">{title}</p>
