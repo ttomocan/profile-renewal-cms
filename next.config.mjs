52596cc33362d1f029fa5ac29f7191e16b8bf1bb
HonokuniTomita <tomita@honokuni-tokai.com>
2024-11-26 17:06:03 +0900

    1126

--- next.config.mjs
+++ next.config.mjs
@@ -9,21 +9,21 @@ const nextConfig = {
     ],
   },
   trailingSlash: true,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/:path*', // 全てのパスを対象にする
-  //       has: [
-  //         {
-  //           type: 'host',
-  //           value: 'tomocan.site', // wwwなしのドメイン名を指定
-  //         },
-  //       ],
-  //       destination: 'https://www.tomocan.site/:path*', // www付きにリダイレクト
-  //       permanent: true, // 301リダイレクト
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/:path*', // 全てのパスを対象にする
+        has: [
+          {
+            type: 'host',
+            value: 'tomocan.site', // wwwなしのドメイン名を指定
+          },
+        ],
+        destination: 'https://www.tomocan.site/:path*', // www付きにリダイレクト
+        permanent: true, // 301リダイレクト
+      },
+    ];
+  },
 };
 
 export default nextConfig;
