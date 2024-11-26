982710b402c31c007b4305a21ac0fb736f2db35a
HonokuniTomita <tomita@honokuni-tokai.com>
2024-11-26 14:51:21 +0900

    1126

--- app/_components/PageTitle/index.tsx
+++ app/_components/PageTitle/index.tsx
@@ -7,14 +7,19 @@ type Props = {
 };
 
 export default function PageTitle({ title, sub, img }: Props) {
+  // 文字列を1文字ずつ分割して <span> タグでラップする関数
+  const wrapWithSpan = (text: string) => text.split('').map((char, index) => <span key={index}>{char}</span>);
+
   return (
     <div className="pagetitle">
       <div className="pagetitle__image">
         <Image src={img} alt="" width={1366} height={400} sizes="100vw" />
       </div>
       <div className="l-inner">
-        <p className="pagetitle__en">{title}</p>
-        <h1 className="pagetitle__ja">{sub}</h1>
+        {/* titleを1文字ずつ<span>で囲む */}
+        <p className="pagetitle__en">{wrapWithSpan(title)}</p>
+        {/* subを1文字ずつ<span>で囲む */}
+        <h1 className="pagetitle__ja">{wrapWithSpan(sub)}</h1>
       </div>
     </div>
   );
