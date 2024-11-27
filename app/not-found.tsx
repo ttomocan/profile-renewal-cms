2aa97f6699cdc273604f8c641952bfae584a61b8
HonokuniTomita <tomita@honokuni-tokai.com>
2024-11-27 12:47:38 +0900

    1127

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,18 @@
+import styles from './not-found.module.css';
+
 export default function NotFound() {
   return (
-    <>
-      <div className="l-inner">
+    <main>
+      <div className={styles.container + 'l-inner'}>
         <dl>
-          <dt>ページが見つかりませんでした</dt>
-          <dd>
+          <dt className={styles.title}>ページが見つかりませんでした</dt>
+          <dd className={styles.text}>
             あなたがアクセスしようとしたページは存在しません。
             <br />
             URLを再度ご確認ください。
           </dd>
         </dl>
       </div>
-    </>
+    </main>
   );
 }
