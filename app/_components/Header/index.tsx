63343334bde5fc9206a68d8dc5e6e030604d6c27
HonokuniTomita <tomita@honokuni-tokai.com>
2024-11-28 09:09:02 +0900

    1128

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import MenuNav from '../MenuNav';
+import { useState } from 'react';
 
 export default function Header() {
+  const [isOpen, setOpen] = useState<boolean>(false);
+  const open = () => setOpen(true);
+  const close = () => setOpen(false);
   return (
     <header className="l-header">
       <h1 className="l-header__logo">
@@ -11,7 +15,7 @@ export default function Header() {
         </Link>
       </h1>
       <div className="l-header__menuBtn">
-        <button className="l-header__menuBtn-button" aria-label="メニューを開く">
+        <button className="l-header__menuBtn-button" aria-label="メニューを開く" onClick={open}>
           <span className="top"></span>
           <span className="middle"></span>
           <span className="bottom"></span>
