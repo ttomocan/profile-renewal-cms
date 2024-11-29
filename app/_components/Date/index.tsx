8fcc656ac441d8f1c29f8f4bacca943114333b56
HonokuniTomita <tomita@honokuni-tokai.com>
2024-11-29 13:41:04 +0900

    1129

--- app/_components/Date/index.tsx
+++ app/_components/Date/index.tsx
@@ -8,9 +8,9 @@ type Props = {
 
 export default function Date({ date }: Props) {
   return (
-    <span className={styles.date}>
+    <time dateTime={formatDate(date)} className={styles.date}>
       <Image src="/img/common/clock.svg" alt="" width={16} height={16} loading="eager" />
       {formatDate(date)}
-    </span>
+    </time>
   );
 }
