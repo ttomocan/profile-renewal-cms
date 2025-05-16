490b6df305c5e028b1cdfdb66f695503f0e489de
HonokuniTomita <tomita@honokuni-tokai.com>
2025-05-16 12:16:39 +0900

    0516

--- app/diary/layout.tsx
+++ app/diary/layout.tsx
@@ -1,24 +1,30 @@
 import './globals.css';
 import '@/styles/pages/diary.scss';
 import PageTitle from '@/app/_components/PageTitle';
+import { getBlogList } from '@/app/_libs/microcms';
+import type { Metadata } from 'next';
 
-export const metadata = {
-  title: '日記',
-  openGraph: {
-    title: 'お問い合わせ',
-  },
-  twitter: {
-    title: 'お問い合わせ',
-  },
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const { contents } = await getBlogList({ limit: 1 });
+  const latestPost = contents[0];
+  const baseImageUrl = latestPost?.thumbnail?.url ?? '/img/common/ogp.png';
+  const version = encodeURIComponent(latestPost?.revisedAt ?? latestPost?.publishedAt ?? Date.now());
+  const imageUrl = `${baseImageUrl}?v=${version}`;
 
-type Props = {
-  children: React.ReactNode;
-};
+  return {
+    title: '日記',
+    openGraph: {
+      title: '日記',
+      images: [imageUrl],
+    },
+    twitter: {
+      title: '日記',
+      images: [imageUrl],
+    },
+  };
+}
 
-export const revalidate = 60;
-
-export default function BlogLayout({ children }: Props) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <PageTitle title="Diary" sub="日記" />
