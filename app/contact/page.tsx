0042788bf9861531f847cdbaf826700ae5a6b5a7
HonokuniTomita <tomita@honokuni-tokai.com>
2025-05-22 17:59:34 +0900

    0522

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -13,12 +13,12 @@ export default async function Page() {
         <li>Webデザインのご相談</li>
         <li>ブログに関する相談</li>
       </ul>
-      <p className="u-mt20 fadeUpTrigger">
+      <p className="u-mt20 u-mb20 fadeUpTrigger">
         など、あなたのお悩みやお困りごとを、下記のお問い合わせフォームからお気軽にご連絡ください。
         <br />
         お問い合わせ内容を確認したら、3日以内に返信します。あなたに合った最適な解決策を一緒に考えて、次のステップに進むサポートしますので、お気軽にご相談ください！
       </p>
-      <p className="c-text-note u-mt20 fadeUpTrigger">お問い合わせの際は、お名前とメールアドレスを必ずご記入ください。記入漏れがありますと、ご返信できない場合がございますので、ご注意をお願いします。</p>
+      <p className="u-text-note fadeUpTrigger">ご返信には、必ずお名前とメールアドレスの記載をお願いいたします。記載がない場合、返信できないことがありますので、あらかじめご了承ください。</p>
       <ContactForm />
     </section>
   );
